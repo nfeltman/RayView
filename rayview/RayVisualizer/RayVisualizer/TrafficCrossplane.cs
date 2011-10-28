122366ef677ddca3cad1ac4527587addad821ab2
Nicolas Feltman <nfeltman@gmail.com>
2011-10-28 10:54:56 -0400

    Traffic Crossplane works relatively well now.  Juat needs some auto-parameter tuning.
    Signed-off-by: Nicolas Feltman <nfeltman@gmail.com>

--- rayview/RayVisualizer/RayVisualizer/TrafficCrossplane.cs
+++ rayview/RayVisualizer/RayVisualizer/TrafficCrossplane.cs
@@ -9,25 +9,20 @@ namespace RayVisualizer
 {
     class TrafficCrossplane : CrossplaneBehavior
     {
-        private int[,] bins;
-        private float step;
-        CVector3 right, up, center; 
-        List<CVector3> traffic;
+        private Dictionary<Tuple<int,int>,int> bins;
+        CVector3 right, up, p;
+
+        private const float BINSIZE = 10;
 
         public void UpdateCrossPlane(SceneData scene)
         {
-            traffic = new List<CVector3>();
+            bins = new Dictionary<Tuple<int, int>, int>();
 
             CVector3 n = scene.ForwardVec.ToC();
-            CVector3 p = scene.Location.ToC() + (n * scene.CrossPlaneDist);
+            p = scene.Location.ToC() + (n * scene.CrossPlaneDist);
 
             right = scene.RightVec.ToC();
             up = right ^ n;
-
-            float minU = float.MaxValue;
-            float maxU = float.MinValue;
-            float minV = float.MaxValue;
-            float maxV = float.MinValue;
 
             //foreach(RaySet set in scene.Rays)
             foreach (RayCast c in scene.Rays[1].Rays)
@@ -39,49 +34,35 @@ namespace RayVisualizer
                     //compute plane-line intersection
                     float t = -a1 / (d * n);
                     CVector3 q = d * t + c.Origin;
-                    traffic.Add(q);
                     float u = (q - p) * right;
                     float v = (q - p) * up;
-                    minU = Math.Min(minU, u);
-                    maxU = Math.Max(maxU, u);
-                    minV = Math.Min(minV, v);
-                    maxV = Math.Max(maxV, v);
+                    int ubin = (int)(u / BINSIZE + .5);
+                    int vbin = (int)(v / BINSIZE + .5);
+                    Tuple<int,int> bin = new Tuple<int,int>(ubin,vbin);
+                    if (bins.ContainsKey(bin))
+                        bins[bin] = bins[bin] + 1;
+                    else
+                        bins[bin] = 1;
                 }
-            }
-
-            step = Math.Max(maxU - minU, maxV - minV)/20;
-            float medU = (minU + maxU) / 2;
-            float medV = (minV + maxV) / 2;
-            center = p + (medU * right) + (medV * up);
-
-            bins = new int[21,21];
-
-            foreach (CVector3 q in traffic)
-            {
-                int u = (int)(((((q - p) * right)-medU)/step)+0.5);
-                int v = (int)(((((q - p) * up)-medV)/step)+0.5);
-                bins[u+10,v+10]++;
             }
         }
 
         public void DrawResults(SceneData scene)
         {
             GL.PointSize(20);
-            GL.Begin(BeginMode.Points);
-            GL.Color4(0, 0, 1, .4);
+            GL.Begin(BeginMode.Quads);
             if (bins != null)
             {
-                //foreach (CVector3 q in traffic)
-                //{
-                    //GL.Vertex3(q.ToGL());
-                //}
-                for(int k=0;k<21;k++)
-                    for(int j=0;j<21;j++)
-                        if (bins[k, j] > 0)
-                        {
-                            //Console.WriteLine(k+" "+j);
-                            GL.Vertex3((center + ((k-10)*step*right) + ((j-10)*step*up)).ToGL());
-                        }
+                foreach (KeyValuePair<Tuple<int,int>,int> kv in bins)
+                {
+                    Tuple<int, int> key = kv.Key;
+                    float val = Math.Min(kv.Value / 10f, 1);
+                    GL.Color4(val, 1-val, 0, .4);
+                    GL.Vertex3((((key.Item1) * BINSIZE * right) + ((key.Item2) * BINSIZE * up) + p).ToGL());
+                    GL.Vertex3((((key.Item1) * BINSIZE * right) + ((key.Item2-1) * BINSIZE * up) + p).ToGL());
+                    GL.Vertex3((((key.Item1-1) * BINSIZE * right) + ((key.Item2-1) * BINSIZE * up) + p).ToGL());
+                    GL.Vertex3((((key.Item1-1) * BINSIZE * right) + ((key.Item2) * BINSIZE * up) + p).ToGL());
+                }
             }
 
             GL.End();

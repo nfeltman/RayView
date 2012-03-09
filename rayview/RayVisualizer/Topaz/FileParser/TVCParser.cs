b1becb6c6eeee7f9aa4f3541c4940bbacb083ab8
Nicolas Feltman <nfeltman@gmail.com>
2012-03-09 00:31:08 -0500

    Added some helper classes that I haven't integrated yet.
    Signed-off-by: Nicolas Feltman <nfeltman@gmail.com>

---
+++ rayview/RayVisualizer/Topaz/FileParser/TVCParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Topaz
+{
+    public class TVCParser
+    {
+        public Dictionary<string, string> GeneralParse(Stream input)
+        {
+            StreamReader reader = new StreamReader(input);
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                int splitIndex = line.IndexOf(": ");
+                if (splitIndex == -1) throw new TopazException("Malformed TVC entry: {0}", line);
+                string key = line.Substring(0, splitIndex);
+                if (dict.ContainsKey(key)) throw new TopazException("TVC file contains duplicate key: {0}", key);
+                dict[key] = line.Substring(splitIndex + 2);
+            }
+            return dict;
+        }
+
+        public class TVCInfo
+        {
+            public string OBJFile;
+            public string RayFile;
+            public string TBCFile;
+        }
+    }
+}
---
+++ rayview/RayVisualizer/Topaz/TopazException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Topaz
+{
+    public class TopazException : Exception
+    {
+        public TopazException(string reason, params object[] args)
+            : base(String.Format(reason,args))
+        {
+
+        }
+    }
+}

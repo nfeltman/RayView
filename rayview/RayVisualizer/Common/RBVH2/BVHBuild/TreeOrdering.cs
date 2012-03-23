a806d96fb35f1cb3d4dbae419017ab5708542a52
Nicolas Feltman <nfeltman@gmail.com>
2012-03-23 17:25:53 -0400

    Slightly adjusted RTSAH to exactly match Thiago's implementation.
    Signed-off-by: Nicolas Feltman <nfeltman@gmail.com>

--- rayview/RayVisualizer/Common/RBVH2/BVHBuild/TreeOrdering.cs
+++ rayview/RayVisualizer/Common/RBVH2/BVHBuild/TreeOrdering.cs
@@ -8,7 +8,7 @@ namespace RayVisualizer.Common
     public class TreeOrdering
     {
         private const float TRAV = 1.0f;
-        private const float PRIM = 1.0f;
+        private const float PRIM = 3.0f;
 
         public static void ApplyRTSAHOrdering<TBranch, TLeaf>(Tree<TBranch, TLeaf> tree)
             where TBranch : Weighted, Boxed
@@ -19,26 +19,26 @@ namespace RayVisualizer.Common
                 {
                     IntersectionReport P = UniformRays.GetReport(br.Content.BBox, left.Box, right.Box);
 
-                    float V = P.JustLeft * left.V + P.JustRight * right.V + P.Both * left.V * right.V + P.Neither;
-                    float C_leftFirst = TRAV + P.Left * left.C + (P.JustRight + P.Both * left.V) * (TRAV + right.C) + P.Neither * TRAV;
-                    float C_rightFirst = TRAV + P.Right * right.C + (P.JustLeft + P.Both * right.V) * (TRAV + left.C) + P.Neither * TRAV;
-                    float C = Math.Min(C_leftFirst, C_rightFirst);
+                    double V = P.JustLeft * left.V + P.JustRight * right.V + P.Both * left.V * right.V + P.Neither;
+                    double C_leftFirst = TRAV + P.Left * left.C + (P.JustRight + P.Both * left.V) * (TRAV + right.C) + P.Neither * TRAV;
+                    double C_rightFirst = TRAV + P.Right * right.C + (P.JustLeft + P.Both * right.V) * (TRAV + left.C) + P.Neither * TRAV;
+                    double C = Math.Min(C_leftFirst, C_rightFirst);
                     // since br.content can be a value type, I have to play some stupid games here to get the copy to carry through
                     br.Content.PLeft = C_leftFirst < C_rightFirst ? 1f : 0f; // go left first if the left cost is lower
                     return new VisibilityRollUp(V, C, br.Content.BBox);
                 },
                 le =>
                 {
-                    return new VisibilityRollUp(le.Content.PrimCount > 0 ? 0.0f : 1.0f, PRIM, le.Content.BBox); 
+                    return new VisibilityRollUp(le.Content.PrimCount > 0 ? 0.0f : 1.0f, TRAV + PRIM * le.Content.PrimCount, le.Content.BBox);
                 });
         }
 
         private class VisibilityRollUp
         {
-            public float V;
-            public float C;
+            public double V;
+            public double C;
             public Box3 Box;
-            public VisibilityRollUp(float v, float c, Box3 box)
+            public VisibilityRollUp(double v, double c, Box3 box)
             {
                 V = v;
                 C = c;
